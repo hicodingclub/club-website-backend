0d26885e617981b215b1b2f19350fe14aba372dd
toliujiayi <toliujiayi@gmail.com>
2020-06-12 17:18:35 -0700

    add summerPrograms module and minor updates for tutors model

---
+++ models/summerProgram/spenrollment-admin.js
@@ -0,0 +1,18 @@
+const schema = require('./spenrollment');
+
+var brief = "name email grade status createdAt";
+var detail = "name email phoneNumber grade status notes adminNotes createdAt";
+var create = "name email phoneNumber status grade notes adminNotes";
+var edit = "name email phoneNumber status grade notes adminNotes";
+var textSearch = "name";
+var index = "name"; //let's temporarily put any field here since this schema is not referred. 
+
+var views = [brief, detail, create, edit, textSearch, index]
+
+//Export model
+module.exports = {
+    schema,
+    views,
+    name: 'Sign Up',
+    api: 'LCRUDEM', //E - export
+};
---
+++ models/summerProgram/spenrollment-public.js
@@ -0,0 +1,37 @@
+const schema = require('./spenrollment');
+
+var brief = "name email grade status createdAt";
+var detail = "name email phoneNumber grade status notes createdAt";
+var create = "name email phoneNumber grade notes";
+var edit = "name email phoneNumber grade notes";
+var textSearch = "name";
+var index = "name"; //let's temporarily put any field here since this schema is not referred. 
+
+var views = [brief, detail, create, edit, textSearch, index]
+
+//Export model
+module.exports = {
+    schema,
+    views,
+    name: 'Sign Up for Summer Program',
+    // patch: ['muser_id'], //field is member which is already defined, so no need to patch
+    api: "C",
+    mraUI: {
+        listType: 'table', // table, list, or grid
+
+        disableListSearch: true, // not show the search bar
+        listActionButtons: ['', ''],
+        listTitle: 'Your Program Enrollments',
+        defaultListSort: { 'createdAt': 'desc' },
+
+        detailActionButtons: ['', '', ''],
+        detailRefName: { // if being referenced, what name to show
+            'Enrollment': 'Enrolled Classes',
+        },
+
+        detailTitle: 'Sign Up for Tutoring',
+        //detailActions: [["Make Payment", "/actions/pay"]],
+
+        selectActionViewType: 'grid', // select extraView type: dropdown, grid, table, list
+    },
+};
